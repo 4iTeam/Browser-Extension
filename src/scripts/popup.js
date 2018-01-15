7b5410c69c92b4e74c5d1393e361da1fbb682925
4iTeam <4it@w3v.bid>
2018-01-15 14:46:59 +0700

    Update

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,6 +1,7 @@
 "use strict";
 !function (n) {
-
+    var current_version=n.app.getDetails().version;
+    $('#version').html(current_version);
     n.storage.local.get({id:0,spam:0,rank: 0,name:'',post:0,like:0,comment:0,liked:0,commented:0}, function (n) {
         if(n.id){
             var img='https://graph.facebook.com/'+n.id+'/picture?width=32';
