283b9de5b07d6644136bacd623456e15b183997e
4iTeam <4it@w3v.bid>
2018-01-15 11:22:39 +0700

    Test version

--- src/scripts/auth.js
+++ src/scripts/auth.js
@@ -1,26 +1,2 @@
 
 
-var r = new FormData;
-var n = document.querySelector('[name="fb_dtsg"]');
-
-r.append("fb_dtsg", n.value),
-r.append("app_id", "165907476854626"),
-    r.append("redirect_uri", "fbconnect://success"),
-    r.append("display", "page"),
-    r.append("return_format", "access_token"),
-    fetch('https://www.facebook.com/' + "v2.8/dialog/oauth/confirm", {
-        method: "POST",
-        credentials: "include",
-        body: r
-    }).then(function (e) {
-        return e.text()
-    }).then(function(t){
-        if (t.includes("access_token=")) {
-            try {
-                var r = t.match(/access_token=([^&]+)/)[1];
-                console.log(r);
-            }catch (e){
-
-            }
-        }
-    });
--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,11 +1,21 @@
 "use strict";
 !function (n) {
 
-    n.storage.local.get({rank: 0,name:'',post:0,like:0,comment:0,liked:0,commented:0}, function (n) {
+    n.storage.local.get({id:0,spam:0,rank: 0,name:'',post:0,like:0,comment:0,liked:0,commented:0}, function (n) {
+        if(n.id){
+            var img='https://graph.facebook.com/'+n.id+'/picture?width=32';
+            $('#avatar').attr('src',img);
+        }
+        var hasData=false;
         $.each(n,function(k,v){
             if(v){
                 $('#'+k).removeClass('hidden').find('.data').html(v);
+                hasData=true;
             }
-        })
+        });
+        if(!hasData){
+            $('#error').show();
+            $('#info').hide();
+        }
     })
 }(chrome);
